74378eb76207552372d564830467f718a43d16c9
komalshinde16 <komalambadasshinde@gmail.com>
2026-03-28 20:16:15 +0530

    initial commit

---
+++ src/main/java/com/email/writer/app/EmailRequest.java
@@ -0,0 +1,15 @@
+package com.email.writer.app;
+
+
+public class EmailRequest {
+    private String emailContent;
+    private String tone;
+
+    public String getEmailContent() {
+        return emailContent;
+    }
+
+    public String getTone() {
+        return tone;
+    }
+}
